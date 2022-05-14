b07384013f0db6b3bc603c2e9207bd8bbfa7f6a6
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-05-14 20:14:08 +0300

    fix bugs in orders retrive

--- lib/pages/orders_view.dart
+++ lib/pages/orders_view.dart
@@ -7,26 +7,34 @@ import '../widgets/app_drawer.dart';
 
 class OrdersView extends StatefulWidget {
   @override
-  State<OrdersView> createState() => approach();
+  State<OrdersView> createState() => _OrdersViewState();
 }
 
-class approach extends State<OrdersView> {
-  bool _isLoading = false;
+class _OrdersViewState extends State<OrdersView> {
+  // bool _isLoading = false; // change to future builder from flutter
+
+  Future _futureProvider;
+  Future _orderFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchOrders();
+  }
 
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((value) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
+    _futureProvider = _orderFuture();
+    // i will go with future builder from flutter
+    // Future.delayed(Duration.zero).then((value) async {
+    //   setState(() {
+    //     _isLoading = true;
+    //   });
+    //   await Provider.of<Orders>(context, listen: false).fetchOrders();
+    //   setState(() {
+    //     _isLoading = false;
+    //   });
+    // });
+    super.initState();
   }
 
-  // another approach
+  // another way of getting data
   //   if (this.mounted) {
   //     setState(() {
   //       _isLoading = true;
@@ -49,18 +57,30 @@ class approach extends State<OrdersView> {
 
   @override
   Widget build(BuildContext context) {
-    final orders = Provider.of<Orders>(context);
+    // final orders = Provider.of<Orders>(context); with this is enabled you will keep in infinite loop. check the course.
     return Scaffold(
         appBar: AppBar(title: const Text('Your Orders')),
         drawer: AppDrawer(),
-        body: _isLoading
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.builder(
-                itemCount: orders.orderItems.length,
-                itemBuilder: (_, i) =>
-                    order_widget.OrderItem(orders.orderItems[i]),
-              ));
+        body: FutureBuilder(
+            future: _futureProvider,
+            builder: (ctx, snapShots) {
+              if (snapShots.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapShots.error != null) {
+                return const Center(
+                  child: Text('Error please try again'),
+                );
+              } else {
+                return Consumer<Orders>(
+                  builder: ((ctx, orders, _) => ListView.builder(
+                        itemCount: orders.orderItems.length,
+                        itemBuilder: (_, i) =>
+                            order_widget.OrderItem(orders.orderItems[i]),
+                      )),
+                );
+              }
+            }));
   }
 }
